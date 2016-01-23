91828664b0f4bc691e1b53adea41a667dd21557b
kpittas <kpittas@snf-687787.(none)>
2016-01-23 15:43:36 +0200

    allages

--- animation.py
+++ animation.py
@@ -13,32 +13,29 @@ class Scatter_animation(object):
 		self.kykloi = 4
 		self.xronos = self.kykloi * np.pi
 		self.x1 = np.linspace(0, self.xronos, self.shmeia)
+		self.x2= np.sin(self.x1) * self.kykloi + self.xronos/1.5
 		self.y1 = np.sin(self.x1)
 		self.y2 = np.cos(self.x1)
 		self.y3 = np.tan(self.x1)/4
 		self.y4 = (1-self.x1*self.x1)**(1/2)
 		self.y5=np.cos(self.x1)*np.sin(self.x1)
 		self.y6=np.cos(self.x1)*np.sin(self.x1)/(np.cos(self.x1)-np.sin(self.x1))
+		self.y7=1-np.sin(self.x2)
 		self.xwork = np.copy(self.x1)
 		self.ywork = np.copy(self.y1)
 		self.mode = 1
-		self.ylist = [(self.x1, self.y1), (self.x1, self.y2), (self.x1, self.y3),(self.x1, self.y5),(self.x1, self.y6)]
-		self.clist = [0.2, 0.1, 0.2,0.1,0.15]
+		self.ylist = [(self.x1, self.y1), (self.x1, self.y2), (self.x1, self.y3),(self.x1, self.y5),(self.x2, self.y7)]
+		self.clist = [1, 1, 0.2,0.1,0.15]
 		self.fig = pyplot.figure()
 		self.ax = self.fig.add_subplot(111)
 		self.ani = animation.FuncAnimation(self.fig, self.update, interval=10,init_func=self.setup_plot, blit=True,repeat=False)
 		random.seed()
 		self.first = sleep
 	def setup_plot(self):
-		self.ax.axis([-0.5, self.xronos+0.5, -self.y*1.1, self.y*1.1])
-		self.scat = self.ax.scatter(self.xwork, self.ywork, c="RED", s=10, animated=True)
+		self.ax.axis([-1, self.xronos+1, -self.y*1.1, self.y*1.1])
+		self.scat = self.ax.scatter(self.xwork, self.ywork, c="RED", s=100, animated=True)
 		self.ax.set_title("TEAM 7 ANIMATION",fontsize=14)
-		a = random(100)*10
-		b = range(100)
-		fig = plt.figure(1)
-		ax = fig.add_subplot(111, axisbg='black')
-		ax.scatter(a,b)
-		fig.canvas.draw()
+		pyplot.style.use('ggplot')
 		return self.scat,  
 	
 	def update(self, i):
@@ -53,7 +50,7 @@ class Scatter_animation(object):
 			 mode = random.randrange(0, len(self.ylist), 1)
 			self.mode = mode	
 		colors = ["red", "orange", "yellow", "green", "blue", "violet"]
-		self.scat = self.ax.scatter(self.xwork, self.ywork, c=random.choice(colors), s=15, animated=True)
+		self.scat = self.ax.scatter(self.xwork, self.ywork, c=random.choice(colors), s=100, animated=True)
 		return self.scat,
 	def show(self):
 		pyplot.show()
